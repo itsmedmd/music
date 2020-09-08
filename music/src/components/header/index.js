3d715c03f887df7dd634577a7deca6ce601dba6c
ElqBell <silenc8@gmail.com>
2020-09-08 17:36:59 +0300

    changed header and added more content to articles

--- music/src/components/header/index.js
+++ music/src/components/header/index.js
@@ -5,27 +5,23 @@ export default function Header() {
 
   return (
     <header className="page-header" >
-        <aside>
-        <img
-          srcSet="header-480w.jpg 480w, header.jpg 910w"
-          sizes="(max-width: 1024px) 480px, 910px"
-          src="header.jpg"
-          alt="A person walking along a relaxing urban city street at night in Japan"
-        />
-        </aside>
         <section>
-            <Song number="01." text="FOMARE" />
-            <Song number="02." text="kiki vivi lily" />
-            <Song number="03." text="Aimer" />
-            <Song number="04." text="Otogivanashi" />
-            <Song number="05." text="Mrs. Green Apple" />
-            <Song number="06." text="Uri Nakaya" />
-            <Song number="07." text="The Cheserasera" />
-            <Song number="08." text="Utada Hikaru" />
-            <Song number="09." text="The Pillows" />
-            <Song number="10." text="Frederic" />
-            <Song number="11." text="Kanako Ito" />
-            <Song number="12." text="Indigo la End" />
+            <div className="song-column">
+              <Song number="01." text="FOMARE" />
+              <Song number="02." text="kiki vivi lily" />
+              <Song number="03." text="Aimer" />
+              <Song number="04." text="Otogivanashi" />
+              <Song number="05." text="Mrs. Green Apple" />
+              <Song number="06." text="Uri Nakaya" />
+            </div>
+            <div className="song-column">
+              <Song number="07." text="The Cheserasera" />
+              <Song number="08." text="Utada Hikaru" />
+              <Song number="09." text="The Pillows" />
+              <Song number="10." text="Frederic" />
+              <Song number="11." text="Kanako Ito" />
+              <Song number="12." text="Indigo la End" />
+            </div>
         </section>
     </header>
   );
--- music/src/pages/index.js
+++ music/src/pages/index.js
@@ -10,110 +10,100 @@ export default function Home() {
     <div className="page-content">
       <Link to=".." className="go-back-to-homepage">GO BACK TO HOME PAGE</Link>
       <Header />
-      <Article className="image-right a1" imgSrc="fomare.jpg" imgAlt="FOMARE">
-        <header>
-          <h1>FOMARE</h1>
-        </header>
-        <section>
-          <h2>Learn more: <a href="http://fomare.com" >fomare.com</a></h2>
-          <h2>Listen to their music: <a href="https://www.youtube.com/channel/UCg5RYtF2Lofxb9L3A3sgitA" >YouTube</a></h2>
-        </section>
-      </Article>
-
-      <Article className="image-right a2" imgSrc="kiki-vivi-lily.jpg" imgAlt="kiki vivi lily">
-        <header>
-          <h1>kiki vivi lily</h1>
-        </header>
-        <section>
-          <h2>Learn more: <a href="https://kikivivilily.com/" >kikivivilily.com</a></h2>
-          <h2>Listen to her music: <a href="https://www.youtube.com/channel/UC2qqLtG-gC_2MHj_-t_F13w" >YouTube</a></h2>
-        </section>
-      </Article>
-
-      <Article className="image-left a10" imgSrc="aimer.jpg" imgAlt="Aimer">
-        <header>
-          <h1>Aimer</h1>
-        </header>
-        <section>
-          <h2>Learn more: <a href="https://www.aimer-web.jp" >aimer-web.jp</a></h2>
-          <h2>Listen to her music: <a href="https://www.youtube.com/channel/UCR1zT1s524Hbc85bdvno_8w" >YouTube</a></h2>
-        </section>
-      </Article>
-
-      <Article className="image-left a5" imgSrc="otogivanashi.jpg" imgAlt="Otogivanashi">
-        <header>
-          <h1>Otogivanashi</h1>
-        </header>
-        <section>
-          <h2>Learn more: <a href="http://otogivanashi.com" >otogivanashi.com</a></h2>
-          <h2>Listen to their music: <a href="https://www.youtube.com/channel/UCd4QzATsDnJqvwG9pmmX6NA" >YouTube</a></h2>
-        </section>
-      </Article>
-
-      <Article className="image-right a6" imgSrc="mrs-green-apple.jpg" imgAlt="Mrs. Green Apple">
-        <header>
-          <h1>Mrs. Green Apple</h1>
-        </header>
-        <section>
-          <h2>Learn more: <a href="https://mrsgreenapple.com" >mrsgreenapple.com</a></h2>
-          <h2>Listen to their music: <a href="https://www.youtube.com/channel/UCpFgmZm65yOU5X-hmWkWjuw" >YouTube</a></h2>
-        </section>
-      </Article>
-
-      <Article className="image-right a3" imgSrc="uri-nakayama.jpg" imgAlt="Uri Nakayama">
-        <header>
-          <h1>Uri Nakayama</h1>
-        </header>
-        <section>
-          <h2>Learn more: <a href="http://nakayamauri.com" >nakayamauri.com</a></h2>
-          <h2>Listen to her music: <a href="https://www.youtube.com/channel/UC1fe2bJKfcR_PFCV7DJWmug/featured" >YouTube</a></h2>
-        </section>
-      </Article>
-
-      <Article className="image-left a7" imgSrc="the-cheserasera.jpg" imgAlt="The Cheserasera">
-        <header>
-          <h1>The Cheserasera</h1>
-        </header>
-        <section>
-          <h2>Learn more: <a href="http://www.thecheserasera.com" >thecheserasera.com</a></h2>
-          <h2>Listen to their music: <a href="https://www.youtube.com/c/TheCheserasera_japan" >YouTube</a></h2>
-        </section>
-      </Article>
-
-      <Article className="image-left a8" imgSrc="utada-hikaru.jpg" imgAlt="Utada Hikaru">
-        <header>
-          <h1>Utada Hikaru</h1>
-        </header>
-        <section>
-          <h2>Learn more: <a href="http://www.utadahikaru.jp" >utadahikaru.jp</a></h2>
-          <h2>Listen to her music: <a href="https://www.youtube.com/user/hikki" >YouTube</a></h2>
-        </section>
-      </Article>
-
-      <Article className="image-right a9" imgSrc="the-pillows.jpg" imgAlt="The Pillows">
-        <header>
-          <h1>The Pillows</h1>
-        </header>
-        <section>
-          <h2>Learn more: <a href="http://pillows.jp" >pillows.jp</a></h2>
-          <h2>Listen to their music: <a href="https://www.youtube.com/channel/UCJFJuYpd7mMeC9mZVdTqV1g" >YouTube</a></h2>
-        </section>
-      </Article>
 
       <Article className="image-right a4" imgSrc="frederic.jpg" imgAlt="Frederic">
-        <header>
-          <h1>Frederic</h1>
-        </header>
+        <header className="header-top"><h1>Frederic</h1></header>
+        <header className="header-bottom"><h1>Frederic</h1></header>
         <section>
           <h2>Learn more: <a href="https://frederic-official.com" >frederic-official.com</a></h2>
           <h2>Listen to their music: <a href="https://www.youtube.com/channel/UCeoCO4_u1DdJBwyil66IsNw" >YouTube</a></h2>
         </section>
       </Article>
 
+      <Article className="image-right a6" imgSrc="mrs-green-apple.jpg" imgAlt="Mrs. Green Apple">
+        <header className="header-top"><h1>Mrs. Green Apple</h1></header>
+        <header className="header-bottom"><h1>Mrs. Green Apple</h1></header>
+        <section>
+          <h2>Learn more: <a href="https://mrsgreenapple.com" >mrsgreenapple.com</a></h2>
+          <h2>Listen to their music: <a href="https://www.youtube.com/channel/UCpFgmZm65yOU5X-hmWkWjuw" >YouTube</a></h2>
+        </section>
+      </Article>
+
+      <Article className="image-left a5" imgSrc="otogivanashi.jpg" imgAlt="Otogivanashi">
+        <header className="header-top"><h1>Otogivanashi</h1></header>
+        <header className="header-bottom"><h1>Otogivanashi</h1></header>
+        <section>
+          <h2>Learn more: <a href="http://otogivanashi.com" >otogivanashi.com</a></h2>
+          <h2>Listen to their music: <a href="https://www.youtube.com/channel/UCd4QzATsDnJqvwG9pmmX6NA" >YouTube</a></h2>
+        </section>
+      </Article>
+
+      <Article className="image-left a8" imgSrc="utada-hikaru.jpg" imgAlt="Utada Hikaru">
+        <header className="header-top"><h1>Utada Hikaru</h1></header>
+        <header className="header-bottom"><h1>Utada Hikaru</h1></header>
+        <section>
+          <h2>Learn more: <a href="http://www.utadahikaru.jp" >utadahikaru.jp</a></h2>
+          <h2>Listen to her music: <a href="https://www.youtube.com/user/hikki" >YouTube</a></h2>
+        </section>
+      </Article>
+
+      <Article className="image-right a1" imgSrc="fomare.jpg" imgAlt="FOMARE">
+        <header className="header-top"><h1>FOMARE</h1></header>
+        <header className="header-bottom"><h1>FOMARE</h1></header>
+        <section>
+          <h2>Learn more: <a href="http://fomare.com" >fomare.com</a></h2>
+          <h2>Listen to their music: <a href="https://www.youtube.com/channel/UCg5RYtF2Lofxb9L3A3sgitA" >YouTube</a></h2>
+        </section>
+      </Article>
+
+      <Article className="image-right a3" imgSrc="uri-nakayama.jpg" imgAlt="Uri Nakayama">
+        <header className="header-top"><h1>Uri Nakayama</h1></header>
+        <header className="header-bottom"><h1>Uri Nakayama</h1></header>
+        <section>
+          <h2>Learn more: <a href="http://nakayamauri.com" >nakayamauri.com</a></h2>
+          <h2>Listen to her music: <a href="https://www.youtube.com/channel/UC1fe2bJKfcR_PFCV7DJWmug/featured" >YouTube</a></h2>
+        </section>
+      </Article>
+
+      <Article className="image-left a10" imgSrc="aimer.jpg" imgAlt="Aimer">
+        <header className="header-top"><h1>Aimer</h1></header>
+        <header className="header-bottom"><h1>Aimer</h1></header>
+        <section>
+          <h2>Learn more: <a href="https://www.aimer-web.jp" >aimer-web.jp</a></h2>
+          <h2>Listen to her music: <a href="https://www.youtube.com/channel/UCR1zT1s524Hbc85bdvno_8w" >YouTube</a></h2>
+        </section>
+      </Article>
+
+      <Article className="image-left a7" imgSrc="the-cheserasera.jpg" imgAlt="The Cheserasera">
+        <header className="header-top"><h1>The Cheserasera</h1></header>
+        <header className="header-bottom"><h1>The Cheserasera</h1></header>
+        <section>
+          <h2>Learn more: <a href="http://www.thecheserasera.com" >thecheserasera.com</a></h2>
+          <h2>Listen to their music: <a href="https://www.youtube.com/c/TheCheserasera_japan" >YouTube</a></h2>
+        </section>
+      </Article>
+
+      <Article className="image-right a2" imgSrc="kiki-vivi-lily.jpg" imgAlt="kiki vivi lily">
+        <header className="header-top"><h1>kiki vivi lily</h1></header>
+        <header className="header-bottom"><h1>kiki vivi lily</h1></header>
+        <section>
+          <h2>Learn more: <a href="https://kikivivilily.com/" >kikivivilily.com</a></h2>
+          <h2>Listen to her music: <a href="https://www.youtube.com/channel/UC2qqLtG-gC_2MHj_-t_F13w" >YouTube</a></h2>
+        </section>
+      </Article>
+
+      <Article className="image-right a9" imgSrc="the-pillows.jpg" imgAlt="The Pillows">
+        <header className="header-top"><h1>The Pillows</h1></header>
+        <header className="header-bottom"><h1>The Pillows</h1></header>
+        <section>
+          <h2>Learn more: <a href="http://pillows.jp" >pillows.jp</a></h2>
+          <h2>Listen to their music: <a href="https://www.youtube.com/channel/UCJFJuYpd7mMeC9mZVdTqV1g" >YouTube</a></h2>
+        </section>
+      </Article>
+
       <Article className="image-left a11" imgSrc="kanako-ito.jpg" imgAlt="Kanako Ito">
-        <header>
-          <h1>Kanako Ito</h1>
-        </header>
+        <header className="header-top"><h1>Kanako Ito</h1></header>
+        <header className="header-bottom"><h1>Kanako Ito</h1></header>
         <section>
           <h2>Learn more: <a href="https://kanataro.amebaownd.com" >kanataro.com</a></h2>
           <h2>Listen to her music: <a href="https://www.youtube.com/channel/UCpFgEr3XUXk_8wK8H9Dn6Cg" >YouTube</a></h2>
@@ -121,9 +111,8 @@ export default function Home() {
       </Article>
 
       <Article className="image-left a12" imgSrc="indigo-la-end.jpg" imgAlt="Indigo la End">
-        <header>
-          <h1>Indigo la End</h1>
-        </header>
+        <header className="header-top"><h1>Indigo la End</h1></header>
+        <header className="header-bottom"><h1>Indigo la End</h1></header>
         <section>
           <h2>Learn more: <a href="https://indigolaend.com" >indigolaend.com</a></h2>
           <h2>Listen to their music: <a href="https://www.youtube.com/channel/UCXKSbzihU5cu0U4qbbjB3GA" >YouTube</a></h2>
